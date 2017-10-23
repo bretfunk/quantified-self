963ba1057c95531d0661a26e122d5c8471f2172c
BFunk <bret.e.funk@gmail.com>
2017-10-23 15:42:35 -0600

    tweaks for github pages

--- main.bundle.js
+++ main.bundle.js
@@ -158,8 +158,8 @@
 /***/ (function(module, exports) {
 
 	module.exports = function () {
-	  //return "https://arcane-depths-57821.herokuapp.com/" // Rails Backend
-	  return "https://localhost:3000/"; // Running Rails app at port:3000
+	  return "https://arcane-depths-57821.herokuapp.com/"; // Rails Backend
+	  //return "http://localhost:3000/" // Running Rails app at port:3000
 	  //return "https://node-api-backend.herokuapp.com/" //node backend on heroku
 	};
 
